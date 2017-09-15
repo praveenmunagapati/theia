6a1d0cb9e8ad61717d26a7314db2e6136758c8de
Akos Kitta <kittaakos@gmail.com>
2017-09-15 09:43:24 +0200

    GH-346: Tried to get rid of the duplicate electron dev dependency.
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- generator-theia/src/common/app-package-generator.ts
+++ generator-theia/src/common/app-package-generator.ts
@@ -29,7 +29,7 @@ export class AppPackageGenerator extends AbstractGenerator {
         const devDependencies = this.isWeb() ? {
             "webpack-dev-server": "^2.5.0"
         } : {
-                "electron": "^1.6.11",
+                "electron-builder": "^19.28.4",
                 "electron-rebuild": "^1.5.11"
             };
         return {
